78523b4dd646121d3c673565dd7eac4f79f2586f
Pedro Reis <pedroreisv@gmail.com>
2017-03-25 19:15:41 -0300

    Render players

--- client/main.js
+++ client/main.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
+
+import { Players } from './../imports/api/players'
+
+const renderPlayers = function (playersList) {
+  return playersList.map((player) => {
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} {player.score > 1 ? 'points' : 'point'}.
+      </p>
+    )
+  })
+}
 
 Meteor.startup(() => {
-  let jsx = <p>Main.js</p>
+  Tracker.autorun(() => {
+    let players = Players.find().fetch()
 
-  ReactDOM.render(jsx, document.getElementById('app'))
+    let jsx = (
+      <div>
+        <h1>Score Keeper</h1>
+        {renderPlayers(players)}
+      </div>
+    )
+
+    ReactDOM.render(jsx, document.getElementById('app'))
+  })
 })
