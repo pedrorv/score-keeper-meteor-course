219792fc26a53c61ab88529f4e72349e62a688ab
Pedro Reis <pedroreisv@gmail.com>
2017-03-26 11:35:07 -0300

    Sort players in descending order of score

--- client/main.js
+++ client/main.js
@@ -9,7 +9,7 @@ import App from './../imports/ui/App'
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch()
+    let players = Players.find({}, { sort: { score: -1 } }).fetch()
 
     ReactDOM.render(
       <App players={players} title="Score Keeper" />, 
