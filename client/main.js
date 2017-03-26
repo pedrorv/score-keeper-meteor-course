142b54f338013c0c9d0b9b54291950b4cb3808b7
Pedro Reis <pedroreisv@gmail.com>
2017-03-26 19:48:06 -0300

    Add different colors for players position.

--- client/main.js
+++ client/main.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 import { Tracker } from 'meteor/tracker'
 
-import { Players } from './../imports/api/players'
+import { Players, calculatePlayersPositions } from './../imports/api/players'
 import App from './../imports/ui/App'
 
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find({}, { sort: { score: -1 } }).fetch()
+    let positionedPlayers = calculatePlayersPositions(players)
 
     ReactDOM.render(
-      <App players={players} title="Score Keeper" />, 
+      <App players={positionedPlayers} title="Score Keeper" />, 
       document.getElementById('app')
     )
   })
--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -4,14 +4,17 @@ import { Players } from './../api/players'
 
 export default class Player extends React.Component {
   render() {
-    const { name, score, _id } = this.props.player
+    const { name, score, _id, position, rank } = this.props.player
+    let itemClassName = `item item--position-${rank}`
 
     return (
-      <div key={_id} className="item">
+      <div key={_id} className={itemClassName}>
         <div className="player">
           <div>
             <h3 className="player__name">{name}</h3>
-            <p className="player__stats">{score} {score > 1 ? 'points' : 'point'}.</p>
+            <p className="player__stats">
+              {position} place: {score} {score > 1 ? 'points' : 'point'}.
+            </p>
           </div>
           <div className="player__actions">
             <button className="button button--round" onClick={() => Players.update({_id: _id}, {$inc: { score: -1 }})}>
